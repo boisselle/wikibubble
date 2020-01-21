f8ef71bd118e09b29448cada2e0a0ba0db0cc2f0
SEAN <augmentedsean@gmail.com>
2020-01-21 14:55:50 -0800

    Added initial web scraper file

---
+++ webscraper.py
@@ -0,0 +1,6 @@
+# This file uses the Beautiful Soup library in order to get Wikipedia data.
+# There are a few APIs that also get it but I have found them to be buggy.
+# The purpose of this file is to test if the beautiful soup library is
+# good for our purposes.
+
+
